84240e9acfcbdc959c2743760b4e34b9df6c6074
Alexey Nikitin <nikitin@bookmate.com>
2018-05-03 15:31:28 +0300

    Port comes as a string when passed as environment variable

--- src/reminder_bot.erl
+++ src/reminder_bot.erl
@@ -6,6 +6,7 @@
 ]).
 
 -define(APPLICATION, reminder_bot).
+-define(DEFAULT_PORT, 8080).
 
 -spec start() -> ok.
 start() ->
@@ -18,4 +19,12 @@ token() ->
 
 -spec port() -> integer().
 port() ->
-    application:get_env(?APPLICATION, port, 8080).
+    case application:get_env(?APPLICATION, port, ?DEFAULT_PORT) of
+        Port when is_list(Port) ->
+            list_to_integer(Port);
+        Port when is_integer(Port) ->
+            Port;
+        Port ->
+            lager:error("Port must be string or integer: [~p].", [Port]),
+            ?DEFAULT_PORT
+        end.
